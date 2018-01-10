c83679796bb66f3f2154670f9cb03bdb4a7300f6
Margarita <margo.himera@yandex.ru>
2018-01-10 23:24:19 +0300

    Recognition service call were moved into NoteActivity

--- app/src/main/java/com/margarita/voicenotes/ui/activities/MainActivity.kt
+++ app/src/main/java/com/margarita/voicenotes/ui/activities/MainActivity.kt
@@ -1,28 +1,17 @@
 package com.margarita.voicenotes.ui.activities
 
-import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import android.content.ActivityNotFoundException
-import android.speech.RecognizerIntent
 import android.content.Intent
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.margarita.voicenotes.R
 import com.margarita.voicenotes.common.NotesAdapter
-import com.margarita.voicenotes.common.showToast
 import com.margarita.voicenotes.models.NoteItem
 import kotlinx.android.synthetic.main.fragment_news.*
-import java.util.*
-
 
 class MainActivity : AppCompatActivity() {
-
-    /**
-     * Request code for recognition activity
-     */
-    private val requestCodeForSpeech = 100
 
     /**
      * Translation Y value for a floating action button for its animation
@@ -60,7 +49,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         setupAdapter()
-        fab.setOnClickListener { startSpeechRecognition() }
+        fab.setOnClickListener {
+            startActivity(Intent(this, NoteActivity::class.java))
+        }
     }
 
     /**
@@ -77,43 +68,5 @@ class MainActivity : AppCompatActivity() {
         }
         adapter.setNotes(notes)
         notesList.adapter = adapter
-    }
-
-    /**
-     * Function for launching speech recognition service
-     */
-    private fun startSpeechRecognition() {
-        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        intent.putExtra(
-                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-        intent.putExtra(
-                RecognizerIntent.EXTRA_LANGUAGE,
-                Locale.getDefault())
-        intent.putExtra(
-                RecognizerIntent.EXTRA_PROMPT,
-                getString(R.string.start_speech))
-        try {
-            startActivityForResult(intent, requestCodeForSpeech)
-        } catch (a: ActivityNotFoundException) {
-            showToast(R.string.speech_not_supported)
-        }
-    }
-
-    /**
-     * Function for receiving speech input
-     */
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == requestCodeForSpeech &&
-                resultCode == Activity.RESULT_OK && data != null) {
-            val intent = Intent(this, NoteActivity::class.java)
-
-            val intentKey = RecognizerIntent.EXTRA_RESULTS
-            val result = data.getStringArrayListExtra(intentKey)
-
-            intent.putStringArrayListExtra(intentKey, result)
-            startActivity(intent)
-        }
     }
 }
--- app/src/main/java/com/margarita/voicenotes/ui/activities/NoteActivity.kt
+++ app/src/main/java/com/margarita/voicenotes/ui/activities/NoteActivity.kt
@@ -1,19 +1,68 @@
 package com.margarita.voicenotes.ui.activities
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.RecognizerIntent
 import android.widget.TextView
 import com.margarita.voicenotes.R
+import com.margarita.voicenotes.common.showToast
 import kotlinx.android.synthetic.main.activity_note.*
+import java.util.*
 
 class NoteActivity : AppCompatActivity() {
+
+    /**
+     * Request code for recognition activity
+     */
+    private val requestCodeForSpeech = 100
+
+    /**
+     * Intent for starting speech recognition service
+     */
+    private val recognitionIntent by lazy {
+        Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                .putExtra(
+                        RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+                .putExtra(
+                        RecognizerIntent.EXTRA_LANGUAGE,
+                        Locale.getDefault())
+                .putExtra(
+                        RecognizerIntent.EXTRA_PROMPT,
+                        getString(R.string.start_speech))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
-        if (intent != null) {
-            val resultArray = intent.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+        //TODO Add launch flag and save it to Bundle
+        //TODO Add function which will check flag and launch service
+        startSpeechRecognition()
+        imgBtnSpeak.setOnClickListener { startSpeechRecognition() }
+    }
+
+    /**
+     * Function for launching speech recognition service
+     */
+    private fun startSpeechRecognition() {
+        try {
+            startActivityForResult(recognitionIntent, requestCodeForSpeech)
+        } catch (a: ActivityNotFoundException) {
+            showToast(R.string.speech_not_supported)
+        }
+    }
+
+    /**
+     * Function for receiving speech input
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == requestCodeForSpeech &&
+                resultCode == Activity.RESULT_OK && data != null) {
+            val resultArray = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
             val speechResult = resultArray[0]
             etNote.setText(speechResult, TextView.BufferType.EDITABLE)
             etNote.setSelection(speechResult.length)
