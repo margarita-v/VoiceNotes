494a2e41c1589431ff4a7026d5a96bbdec11104f
Margarita <margo.himera@yandex.ru>
2018-01-15 16:36:35 +0300

    Multichoice with check boxes and long click was implemented

--- app/src/main/java/com/margarita/voicenotes/common/NotesAdapter.kt
+++ app/src/main/java/com/margarita/voicenotes/common/NotesAdapter.kt
@@ -1,17 +1,18 @@
 package com.margarita.voicenotes.common
 
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import android.view.ViewGroup
 import com.margarita.voicenotes.R
 import com.margarita.voicenotes.models.NoteItem
 import com.margarita.voicenotes.models.NoteViewModel
-import com.margarita.voicenotes.ui.holders.NoteViewHolder
+import kotlinx.android.synthetic.main.item_note.view.*
 
 /**
  * Adapter for a note items list
  */
-class NotesAdapter(private val noteClickListener: OnNoteClickListener):
-        RecyclerView.Adapter<NoteViewHolder>() {
+class NotesAdapter(private val noteClickListener: OnNoteClickListener)
+    : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
 
     /**
      * List of notes
@@ -51,6 +52,15 @@ class NotesAdapter(private val noteClickListener: OnNoteClickListener):
      * @param position Position of selected note item
      */
     fun selectItem(position: Int) {
+        checkItem(position)
+        notifyItemChanged(position)
+    }
+
+    /**
+     * Function for a note item checking
+     * @param position Position of checked note item
+     */
+    fun checkItem(position: Int) {
         // Change item's checked state
         val prevChecked = notes[position].checked
         notes[position].checked = !prevChecked
@@ -67,7 +77,47 @@ class NotesAdapter(private val noteClickListener: OnNoteClickListener):
             // Check if the last item was deselected
             isMultiChoiceMode = checkedItemsCount > 0
         }
-        notifyItemChanged(position)
+    }
+
+    /**
+     * View holder for a note item
+     */
+    inner class NoteViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        /**
+         * Function for binding a noteItem item and showing its info in view
+         * @param noteViewModel Note which info will be shown
+         * @param position Position of chosen note
+         * @param noteClickListener Listener of note click events
+         */
+        fun bind(noteViewModel: NoteViewModel,
+                 position: Int,
+                 noteClickListener: NotesAdapter.OnNoteClickListener) = with(itemView) {
+
+            val noteItem = noteViewModel.noteItem
+            setOnClickListener { noteClickListener.onNoteClick(noteItem) }
+            setOnLongClickListener { noteClickListener.onNoteLongClick(position) }
+            tvDescription.text = noteItem.description
+            tvDate.text = noteItem.parseDate()
+            checkBox.setOnCheckedChangeListener(null)
+            checkBox.isChecked = noteViewModel.checked
+            setupCheckedUI()
+            if (isMultiChoiceMode) {
+                checkBox.setOnCheckedChangeListener { _, _ ->
+                    checkItem(position)
+                    setupCheckedUI()
+                }
+            }
+        }
+
+        /**
+         * Function for UI setup for a current checked state
+         */
+        private fun setupCheckedUI() {
+            with(itemView) {
+                checkBox.visibility = if (checkBox.isChecked) View.VISIBLE else View.GONE
+            }
+        }
     }
 
     /**
